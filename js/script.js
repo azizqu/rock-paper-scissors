a09fd01f2e26489bd2ed3f0289bd89af19c2f4b1
aziz qureshi <azizqu@gmail.com>
2021-09-25 18:54:34 -0400

    fixed round logic icrementing incorrectly, formtatted results displayed

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ console.log("hello")
 // TODO: keep track of ties
 let win = 0;
 let loss = 0;
-// let ties = 0;
-let round = 1;
+let ties = 0;
+let round =1;
+let round_play = 0
 
 let choice = [
     "Rock",
@@ -18,7 +19,7 @@ function startNewGame(event) {
     gameWindow.classList.remove('hidden');
     win = 0;
     loss = 0;
-    round = 1;
+    //round++;
 }
 // refreshes page and reloads init values
 function resetGame(event) {
@@ -32,6 +33,7 @@ function resetGame(event) {
     win = 0;
     loss = 0;
     round = 1;
+    round_play = 0;
     roundNumber.innerHTML = round;
     playButton.removeAttribute('disabled');
     resultsWindow.innerHTML = '';
@@ -59,6 +61,7 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    round++
 
     // TODO: check if the user has "please select your weapon (-1) value"
     // do not move forward if wrong drop down is selected
@@ -66,25 +69,25 @@ function playRound(playerSelection, computerSelection) {
     // check the increment, incrementing too early
 
     if (playerSelection === computerSelection) {
-        return (round++, "It is a tie!<br> " + "End of Round: " + round + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, ties++, "It is a tie!<br> " + "End of Round: " + round_play + "<br> " +" Wins: " + win + " Losses: " + loss + " Ties: " + ties) 
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return (round++, win++, "You Win!<br> " + " End of Round: " + round + " " + playerSelection + " Beats " + computerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, win++, "You Win!<br> " + " End of Round: " + round_play + "<br> " + playerSelection.toUpperCase() + " Beats " + computerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return (round++, loss++, "You Lose!<br> " + " End of Round: " + round + " " + computerSelection + " Beats " + playerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, loss++, "You Lose!<br> " + " End of Round: " + round_play + "<br> " + computerSelection.toUpperCase() + " Beats " + playerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        return (round++, win++, "You Win!<br> " + " End of Round: " + round + " " + playerSelection + " Beats " + computerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, win++, "You Win!<br> " + " End of Round: " + round_play + "<br> " + playerSelection.toUpperCase() + " Beats " + computerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        return (round++, loss++, "You Lose!<br> " + " End of Round: " + round + " " + computerSelection + " Beats " + playerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, loss++, "You Lose!<br> " + " End of Round: " + round_play + "<br> " + computerSelection.toUpperCase() + " Beats " + playerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return (round++, win++, "You Win!<br> " + " End of Round: " + round + " " + playerSelection + " Beats " + computerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, win++, "You Win!<br> " + " End of Round: " + round_play + "<br> " + playerSelection.toUpperCase() + " Beats " + computerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return (round++, loss++, "You Lose!<br> " + " End of Round: " + round + " " + computerSelection + " Beats " + playerSelection + " Wins: " + win + " Losses: " + loss)
+        return (round_play++, loss++, "You Lose!<br> " + "End of Round: " + round_play + "<br> " + computerSelection.toUpperCase() + " Beats " + playerSelection.toUpperCase() + "<br> Wins: " + win + " Losses: " + loss + " Ties: " + ties)
     }
 
 }
@@ -97,24 +100,40 @@ function game() {
     let playerSelection = madeMyChoice(this);
     let computerSelection = computerPlay();
     document.getElementById("round-number").innerHTML = round;
+    linebreak = document.createElement("br");
+
 
     resultsWindow.innerHTML = playRound(playerSelection, computerSelection);
 
     if (round < 5) {
         console.log("End of Round " + round + " Start another game...")
     }
-    else if (round === 5) {
+    else if (round_play === 5) {
         playButton.setAttribute('disabled', true);
         resultsWindow.innerHTML = "End of game <br>";
-        resultsWindow.append("Game Results: " + " Player Wins: " + win + " Computer wins: " + loss)
+        resultsWindow.append("Game Results: ")
+        resultsWindow.append(linebreak)
+        resultsWindow.append(" Player Wins: " + win)
+        resultsWindow.append(linebreak)
+        resultsWindow.append(" Computer wins: " + loss)
+        resultsWindow.append(linebreak)
+        resultsWindow.append(" Ties: " + ties)
+
+
         if (win > loss) {
             console.log("Congrats you won best of 5!! ");
+            resultsWindow.append(linebreak)
+            resultsWindow.append("Congrats you won best of 5!! ")
         }
         else if (win < loss) {
             console.log("Sorry you lose best of 5!! ");
+            resultsWindow.append(linebreak)
+            resultsWindow.append("Sorry you lose best of 5!! ")
         }
         else if (win === loss) {
             console.log("It is a tie! ");
+            resultsWindow.append(linebreak)
+            resultsWindow.append("Nobody wins it is a tie!! ")
         }
     }
 
